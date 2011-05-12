95b2f3dd874581602017f2f345a3d028615138e9
Root on 38 <root@evresis.com>
2011-05-12 15:23:06 +0300

    working like a charm

--- components/EModuleActiveRecord.php
+++ components/EModuleActiveRecord.php
@@ -3,8 +3,7 @@ class EModuleActiveRecord extends CActiveRecord
 {
     public function getDbConnection()
     {
-        $moduleDb = new CDbConnection(Yii::app()->controller->module->db);
-        $moduleDb->active = true;
-        return $moduleDb;
+        $db = Yii::app()->controller->module->db;
+        return Yii::createComponent($db);
     }
 }
